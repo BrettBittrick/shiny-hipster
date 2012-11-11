ab208b418f3350e42c59bfdabf2f9d2df24bb0e9
Brett Bittrick <brett.bittrick@gmail.com>
2012-11-11 02:09:13 -0800

    Messaging
    Added a basic class for messaging

--- src/main.py
+++ src/main.py
@@ -1,33 +1,48 @@
 print "Welcome to RPG!"
+
+class Messenger :
+	message = "No Message"
+
+	def __init__(self):
+		print "Printing"
+
+	def aoran(self,str):
+		##determines whether or not a word should be preceeded by an a or an an
+		if (str[0] == 'a' or str[0] == 'e' or str[0] == 'i' or str[0] == 'o' or str[0] == 'u') :
+			return "an %s" % str 
+		else :
+			return "a %s" % str
+	def appear(self,actor) :
+		print "%s has appeared!" % self.aoran(actor.name)
+
 class Actor :
 	# anything that can participate in combat
+	name = "mumei"
 	init = 0
 	actInit = 100
-	def __init__(self):
-		print "Actor created!"
+	curHP = 100
+	maxHP = 100
+	atk = 10
+
+	def __init__(self,name):
+		self.name = name
 		init = 0
 		actInit = 100
+		message = Messenger()
+		message.appear(self)
 
 	def advance(self) :
-		print "Advancing"
 		self.init += 1
 
 	def act(self) :
-		print "acting"
 		self.advance()
 		if (self.init == self.actInit) :
-			print "Acting"
 			self.init = 0
-		else :
-			print "Only %i of %i init, not acting" % (self.init, self.actInit)	
 
 exit = False
 turn = 0
 
-
-x = Actor()
+x = Actor("Actor x")
 while (exit != True) : 
-	print "Game Loop"
 	x.act()
-	print "after x.act"
 
