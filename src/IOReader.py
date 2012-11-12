8e59f846ea59617c670de162115280c0b9d4e2b1
Brett Bittrick <brett.bittrick@gmail.com>
2012-11-11 22:26:58 -0800

    IO class
    Added a basic IO class for reading values from files.  Really terrible
    at the moment.

---
+++ src/IOReader.py
@@ -0,0 +1,46 @@
+class IOReader :
+	"""lets you perform actions with files"""
+
+	f = "Error"
+	def __init__(self,path) :
+		self.f = open(path, 'r')
+
+	def parse(self) :
+		"""returns a dictionary full of keys and values to be sorted by the 
+		caller"""
+		keys = {}
+
+		for line in self.f :
+			k = self.grabKey(line)
+			keys.update(k)
+			
+		print "Grabbed keys, here's what we have:"
+		print str(keys.items())
+
+	def grabKey(self,line):
+		#Accumulates characters into a string until it comes across a deliniator
+		keys = {}
+		key = ""
+		value = ""
+		keyFound = False
+		#this is bad and I should feel bad
+		qCount = 0
+		for c in line :
+			if not (keyFound) :
+				if not (c == ":" or c == "\n"):
+					key = key + c
+				else :
+					keys[key] = "novalyet"
+					keyFound = True
+			elif not (c == "\n"):
+				if (c == " " or c == "\t"):
+					#strip tabs and other shit from the output
+					continue
+				#try and just get between the quotes
+				elif (c == "\""):
+					qCount += 1
+				elif (qCount != 2):
+					value = value + c
+
+		keys = {key:value}
+		return keys
